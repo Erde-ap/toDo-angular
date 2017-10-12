10b72a5b395fe412151df02a16f624b2df826b89
Erde-ap <mtdaier@gmail.com>
2017-10-12 21:05:49 +0900

    プロトタイプ完成 今後の課題:コンポーネント分割とサービスの作成

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,33 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent{
   title = 'app';
-  listItem = "";
+  listItem:string = ``;
+  inputValue = "";
+  safeListItem:SafeHtml;
 
   addItem(input:string){
-    this.listItem += `<li class="list-group-item">${input}</li>`;
+    if (input == "") {
+      return false;
+    }else{
+      this.listItem += `
+      <li class="todo-item-group todo-item">
+      <label>${input}</label>
+      <span class="input-group-btn">
+      <button class="btn btn-outline-info">完了</button>
+      </span>
+      </li>`;
+      this.inputValue ="";
+      return this.safeListItem = this.sanitizer.bypassSecurityTrustHtml(this.listItem);
+    }
   }
-
+  constructor(private sanitizer: DomSanitizer) {
+  }
 }
